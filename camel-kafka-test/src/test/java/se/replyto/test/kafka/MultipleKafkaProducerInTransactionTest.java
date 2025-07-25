f471bab09b92b2b6c32b249d3d171a8ab390f8c5
Håkan Lantz <hakan.lantz@replyto.se>
2025-07-25 12:21:10 +0200

    Added HappyPathWithSingleKafkaProcucer test

--- camel-kafka-test/src/test/java/se/replyto/test/kafka/MultipleKafkaProducerInTransactionTest.java
+++ camel-kafka-test/src/test/java/se/replyto/test/kafka/MultipleKafkaProducerInTransactionTest.java
@@ -3,6 +3,8 @@ package se.replyto.test.kafka;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 
 import java.util.Properties;
@@ -10,13 +12,17 @@ import java.util.Properties;
 import org.apache.camel.CamelContext;
 import org.apache.camel.CamelExecutionException;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaClientFactory;
 import org.apache.camel.component.kafka.KafkaComponent;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -30,7 +36,9 @@ public class MultipleKafkaProducerInTransactionTest extends CamelTestSupport {
 	
 	private MockProducer<String, String> mockProducer = new MockProducer<>(true, new StringSerializer(), new StringSerializer());
 			
-	@Override
+    private Throwable exceptionCaught = null; 
+
+    @Override
 	protected CamelContext createCamelContext() throws Exception {
 		CamelContext context = super.createCamelContext();
 		
@@ -54,13 +62,41 @@ public class MultipleKafkaProducerInTransactionTest extends CamelTestSupport {
 				.append("* Testing HappyPath *").append(System.lineSeparator()) //
 				.append("*********************").append(System.lineSeparator()));
 		
+		exceptionCaught = null;
+		
+		try {
+			testHappyPath("Hello World!", false);
+		} catch (CamelExecutionException e) {
+			System.err.printf("Unexpected %s thrown by route with message: %s", e.getClass().getSimpleName(), e.getMessage());
+		}
+		
+		assertNull(exceptionCaught, String.format("Unexpected %s caught in route with message: %s", exceptionCaught.getClass().getSimpleName(), exceptionCaught.getMessage()));
+	}
+	
+	@Test
+	public void test02_HappyPathWithSingleKafkaProcucer() throws Exception {
+		System.out.print(new StringBuilder(System.lineSeparator()) //
+				.append("***********************************************").append(System.lineSeparator()) //
+				.append("* Testing HappyPath with single KafkaProcucer *").append(System.lineSeparator()) //
+				.append("***********************************************").append(System.lineSeparator()));
+		
+		testHappyPath("topic1:test1;topic2:test2", true);
+	}
+
+	private void testHappyPath(String body, boolean useSingleKafkaProcucer) throws InterruptedException {
 		doneEndpoint.setExpectedCount(1);
 
-		template.sendBody("direct:start", "Hello World!");
+		template.sendBodyAndHeader("direct:start", body, "UseSingleKafkaProcucer", useSingleKafkaProcucer);
 
 		MockEndpoint.assertIsSatisfied(context);
 
 		assertEquals(2, mockProducer.history().size());
+		ProducerRecord<String, String> record1 = mockProducer.history().get(0);
+		assertEquals("topic1", record1.topic());
+		assertEquals("test1", record1.value());
+		ProducerRecord<String, String> record2 = mockProducer.history().get(1);
+		assertEquals("topic2", record2.topic());
+		assertEquals("test2", record2.value());
 		assertEquals(1, mockProducer.commitCount());
 	}
 	
@@ -71,10 +107,36 @@ public class MultipleKafkaProducerInTransactionTest extends CamelTestSupport {
 			@Override
 			public void configure() throws Exception {
 				from("direct:start")
-					.setBody(simple("test1 ${exchangeId}"))
-					.to("kafka:topic1?additional-properties[transactional.id]=1234&additional-properties[enable.idempotence]=true&additional-properties[retries]=5")
-					.setBody(simple("test2 ${exchangeId}"))
-					.to("kafka:topic2?additional-properties[transactional.id]=1234&additional-properties[enable.idempotence]=true&additional-properties[retries]=5")
+					.doTry()
+						.choice()
+							.when(header("UseSingleKafkaProcucer"))
+								.split(body().tokenize(";")).shareUnitOfWork(true).stopOnException()
+									.setBody(exchange -> {
+										Message message = exchange.getMessage();
+										String body = message.getBody(String.class);
+										String[] split = body.split(":");
+										
+										message.removeHeaders(".*");
+										message.setHeader(KafkaConstants.OVERRIDE_TOPIC, split[0]);
+										return split[1];
+									})
+									.toD("kafka:topic?additional-properties[transactional.id]=1234&additional-properties[enable.idempotence]=true&additional-properties[retries]=5")
+								.end() // .split
+							.endChoice()
+							.otherwise()
+								.setBody(constant("test1"))
+								.toD("kafka:topic1?additional-properties[transactional.id]=1234&additional-properties[enable.idempotence]=true&additional-properties[retries]=5")
+								.setBody(constant("test2"))
+								.toD("kafka:topic2?additional-properties[transactional.id]=1234&additional-properties[enable.idempotence]=true&additional-properties[retries]=5")
+						.end() // .choice()
+					.endDoTry()
+					.doCatch(Throwable.class)
+						.process(exchange -> {
+							exceptionCaught = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+							exceptionCaught.printStackTrace();
+						})
+						.markRollbackOnly()
+					.end() // .doTry()
 					.to("mock:done");
 
 			}
